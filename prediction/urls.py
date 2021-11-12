f4e97afdc30d7024341d29a25059937b0466e118
thisispawann <pawanstha.6964@gmail.com>
2021-11-12 15:55:59 +0545

    logout

--- prediction/urls.py
+++ prediction/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('home/', home, name='home'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- prediction/views.py
+++ prediction/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -21,7 +22,7 @@ def register(request):
 
     return render(request, 'registration/register.html', {'form': f})
 
-
+@login_required
 #Home
 def home(request):
     return render(request, "home.html")
