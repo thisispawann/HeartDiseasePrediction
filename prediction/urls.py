34b648f1c504a0ccb8cbde14e1d4535f19cdb801
thisispawann <pawanstha.6964@gmail.com>
2021-11-12 16:10:03 +0545

    change password

--- prediction/urls.py
+++ prediction/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('home/', home, name='home'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('change_password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
+    path('password_change_done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
 ]
